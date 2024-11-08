0cfa6ac8c303e41d6512a86a424866e3cc3aa3f5
周云龙 <yunlongah@qq.com>
2024-11-08 18:50:11 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="root">
+
+  </div>
+</template>
+
+<style scoped>
+.root {
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  overflow: hidden;
+  user-select: none;
+}
+</style>
